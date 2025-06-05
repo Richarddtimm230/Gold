52de0aaac4908ca9563578bed2b5be36b97f6d67
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-06-05 23:31:19 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectDB = async () => {
-  try {
-    // Replace the connection string below with your MongoDB Atlas URI
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    console.log('MongoDB Atlas Connected');
-  } catch (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDB;
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Atlas Connected'))
+  .catch(err => console.error('MongoDB connection error:', err));

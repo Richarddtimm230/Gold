e838448fca8b7d4946d5fa3e4e7cfa64c38e5dac
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-06-06 00:12:05 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,5 @@
 const mongoose = require('mongoose');
 const uri = process.env.MONGODB_URI;
-
-function connectDB() {
-  return mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-}
-
-module.exports = connectDB;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Atlas Connected'))
+  .catch(err => console.error('MongoDB connection error:', err));

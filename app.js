6912a3fc3bfa3ce675cd8c2c11498ba3003c8b1d
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-06-06 00:11:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const connectDB = require('./db');
+require('./db'); // Just require, do NOT call as a function!
 
 const resultsRoute = require('./routes/results');
 
 const app = express();
-
-// Middleware
 app.use(express.json());
-
-// Connect to MongoDB Atlas
-connectDB();
-
-// Routes
 app.use('/api/results', resultsRoute);
 
 app.get('/', (req, res) => {
   res.send('School Results API running');
 });
 
-// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

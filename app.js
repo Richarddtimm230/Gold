057064991aab6226194e361135c730e90dc877b9
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-06-06 00:24:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));

c70cc699c493049ad7ef39000f6893a5b351078c
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-06-06 01:55:35 +0100

    Update Class.js

--- models/Class.js
+++ models/Class.js
@@ -1,7 +1,16 @@
+
 const mongoose = require('mongoose');
 
-const ClassSchema = new mongoose.Schema({
-  name: { type: String, required: true } // e.g., 'JSS1'
+const teacherSchema = new mongoose.Schema({
+  name: String,
+  arm: String // optional
+}, { _id: false });
+
+const classSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  arms: [String],
+  subjects: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Subject' }],
+  teachers: [teacherSchema]
 });
 
-module.exports = mongoose.model('Class', ClassSchema);
+module.exports = mongoose.model('Class', classSchema);

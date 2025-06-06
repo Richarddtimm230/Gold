25ce9a00c0dabfe2433afb6d0e3b4a0f2aa6a517
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-06-06 08:42:43 +0100

    Update ensureSuperAdmin.js

--- utils/ensureSuperAdmin.js
+++ utils/ensureSuperAdmin.js
@@ -1,28 +1,26 @@
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
-// This function ensures the superadmin user is (re)created on startup.
-// It deletes any existing superadmin(s), then creates a fresh one using environment variables.
 async function ensureSuperAdmin() {
   const superEmail = process.env.SUPERADMIN_EMAIL || 'superadmin@goldlincschools.com';
   const superPassword = process.env.SUPERADMIN_PASSWORD || 'SuperSecurePassword123!';
   const superName = process.env.SUPERADMIN_NAME || 'Super Admin';
 
-  // Remove ALL existing superadmins (force recreate for debugging/fresh setup)
-  await User.deleteMany({ role: 'superadmin' });
-
-  // Hash the new password
   const hash = await bcrypt.hash(superPassword, 10);
 
-  // Create the new superadmin
-  await User.create({
-    name: superName,
-    email: superEmail,
-    password: hash,
-    role: 'superadmin'
-  });
+  // Update if exists, create if not
+  await User.findOneAndUpdate(
+    { email: superEmail },
+    {
+      name: superName,
+      email: superEmail,
+      password: hash,
+      role: 'superadmin'
+    },
+    { upsert: true, new: true }
+  );
 
-  console.log('\x1b[32m%s\x1b[0m', '==> Superadmin CREATED');
+  console.log('\x1b[32m%s\x1b[0m', '==> Superadmin ensured');
   console.log('Email:', superEmail);
   console.log('Password:', superPassword);
 }

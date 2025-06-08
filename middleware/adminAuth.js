16d57f5d1a6d819075e386c79dc9ae11ae00cfcf
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-06-08 07:19:24 +0100

    Create adminAuth.js

---
+++ middleware/adminAuth.js
@@ -0,0 +1,6 @@
+module.exports = function (req, res, next) {
+  if (!req.user || !['admin', 'staff', 'superadmin'].includes(req.user.role)) {
+    return res.status(403).json({ error: 'Forbidden: Admin/Staff access only.' });
+  }
+  next();
+};

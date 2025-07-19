11955a9dac604e954602e327d8b733b7421ea0e8
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-07-19 08:43:02 +0100

    Update dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const express = require('../models/Student');
+const Student = require('../models/Student');
 const {
   
   Employee,

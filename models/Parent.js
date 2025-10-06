03e947be27ace04d9b785c73b828ac389563a2db
richardtim235 <ochukorichard@student.oauife.edu.ng>
2025-10-06 15:34:53 +0100

    Create Parent.js

---
+++ models/Parent.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const ParentSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  contact: { type: String },
+  families: [{ type: String }], // Names or Family IDs (for now, names)
+}, { timestamps: true });
+
+module.exports = mongoose.model('Parent', ParentSchema);

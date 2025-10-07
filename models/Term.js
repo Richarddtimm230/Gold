d350da2ec8a896d643b626c31070ae166b49a175
MumSi042024 <richardochuko83@gmail.com>
2025-10-07 15:43:03 +0100

    Update Term.js

--- models/Term.js
+++ models/Term.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const TermSchema = new mongoose.Schema({
-  name: { type: String, required: true }, // e.g., 'FIRST TERM'
-  is_active: { type: Boolean, default: true }
-});
+const TermSchema = new Schema({
+  name: { type: String, required: true },
+  session: { type: Schema.Types.ObjectId, ref: 'Session', required: true },
+  startDate: { type: Date },
+  endDate: { type: Date }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Term', TermSchema);

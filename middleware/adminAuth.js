49d44375752feb00508d2817168b6c258d54dc57
Richarddtimm230 <oluwaferanmialex2@gmail.com>
2025-10-07 12:11:13 +0100

    Update adminAuth.js

--- middleware/adminAuth.js
+++ middleware/adminAuth.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-  if (!req.user || !['admin', 'teacher', 'staff', 'superadmin'].includes(req.user.role)) {
+  if (!req.user || !['admin', 'teacher', 'Teacher', 'staff', 'superadmin'].includes(req.user.role)) {
     return res.status(403).json({ error: 'Forbidden: Admin/Staff access only.' });
   }
   next();

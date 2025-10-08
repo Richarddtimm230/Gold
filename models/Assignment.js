b3cc375a30e24e424ecdb1f27eae407a6cd55f5b
JoeMarvis2024 <joemarvis2024@gmail.com>
2025-10-08 05:46:26 +0100

    Update Assignment.js

--- models/Assignment.js
+++ models/Assignment.js
@@ -5,7 +5,8 @@ const AssignmentSchema = new mongoose.Schema({
   description: { type: String, default: "" },
   subject: { type: mongoose.Schema.Types.ObjectId, ref: 'Subject', required: true },
   assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Student' }],
-  class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true }, // CHANGED
+  class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true },
+  teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff', required: true }, // <--- ADD THIS LINE
   files: [{ url: String, name: String }],
   dueDate: { type: Date, required: true },
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'Staff' },

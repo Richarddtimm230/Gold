7c2ca51bd00eb601b29feab19ca180c92376af1c
christyfavour2024 <christyfavour2024@gmail.com>
2025-10-10 04:19:41 +0100

    Update reportPreferences.js

--- routes/reportPreferences.js
+++ routes/reportPreferences.js
@@ -14,8 +14,23 @@ router.get('/', adminAuth, async (req, res) => {
 
 // Update preferences
 router.post('/', adminAuth, async (req, res) => {
+  // Only update fields defined in the schema (prevents accidental document pollution)
+  const allowedFields = [
+    "showTeacherRemark", "showPsychomotorSkills", "showAffectiveSkills", "showAttendance",
+    "showPrincipalComment", "showSubjectPosition", "showGradePoints", "showGPA",
+    "showResultSummary", "showStudentPhoto", "showClassSize", "showStudentAge",
+    "showStudentEmail", "showNextTermDate", "showPrintedDate", "showSchoolMotto",
+    "showSchoolLogo", "showCoatOfArms", "showKeyToGrades", "showRedSeal",
+    "showAttendanceNoOfDays", "showAttendancePresent", "showAttendanceAbsent",
+    "showAttendancePercent", "showResultSummaryLabel"
+  ];
+  const update = {};
+  allowedFields.forEach(field => {
+    if (typeof req.body[field] !== 'undefined') update[field] = req.body[field];
+  });
+
   let prefs = await ReportPreference.findByIdAndUpdate(
-    "global", req.body, { upsert: true, new: true }
+    "global", update, { upsert: true, new: true }
   );
   res.json(prefs);
 });

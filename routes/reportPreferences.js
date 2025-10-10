c00d511d48c42f60031fd7c7de72d8d29830f826
christyfavour2024 <christyfavour2024@gmail.com>
2025-10-10 05:00:47 +0100

    Update reportPreferences.js

--- routes/reportPreferences.js
+++ routes/reportPreferences.js
@@ -14,23 +14,19 @@ router.get('/', adminAuth, async (req, res) => {
 
 // Update preferences
 router.post('/', adminAuth, async (req, res) => {
-  // Only update fields defined in the schema (prevents accidental document pollution)
+  // Only allow update of defined fields to prevent unwanted keys
   const allowedFields = [
     "showTeacherRemark", "showPsychomotorSkills", "showAffectiveSkills", "showAttendance",
-    "showPrincipalComment", "showSubjectPosition", "showGradePoints", "showGPA",
-    "showResultSummary", "showStudentPhoto", "showClassSize", "showStudentAge",
-    "showStudentEmail", "showNextTermDate", "showPrintedDate", "showSchoolMotto",
-    "showSchoolLogo", "showCoatOfArms", "showKeyToGrades", "showRedSeal",
-    "showAttendanceNoOfDays", "showAttendancePresent", "showAttendanceAbsent",
-    "showAttendancePercent", "showResultSummaryLabel"
+    "showPrincipalComment", "showSubjectPosition", "showGradePoints", "showGPA", "showResultSummary",
+    "showStudentPhoto", "showClassSize", "showStudentAge", "showStudentEmail", "showNextTermDate",
+    "showPrintedDate", "showSchoolMotto", "showSchoolLogo", "showCoatOfArms", "showKeyToGrades",
+    "showRedSeal", "showAttendanceNoOfDays", "showAttendancePresent", "showAttendanceAbsent", "showAttendancePercent"
   ];
-  const update = {};
-  allowedFields.forEach(field => {
-    if (typeof req.body[field] !== 'undefined') update[field] = req.body[field];
-  });
+  const toUpdate = {};
+  allowedFields.forEach(f => { if (f in req.body) toUpdate[f] = req.body[f]; });
 
   let prefs = await ReportPreference.findByIdAndUpdate(
-    "global", update, { upsert: true, new: true }
+    "global", toUpdate, { upsert: true, new: true }
   );
   res.json(prefs);
 });

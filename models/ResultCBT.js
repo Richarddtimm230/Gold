507b16d26dab5d19f1817ffecb51355d2b39de4e
marythomas02024 <marythomas02024@gmail.com>
2025-10-18 12:34:26 +0100

    Add class reference to resultCBTSchema

--- models/ResultCBT.js
+++ models/ResultCBT.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const resultCBTSchema = new mongoose.Schema({
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
   exam: { type: mongoose.Schema.Types.ObjectId, ref: 'Exam', required: true },
+  class: { type: mongoose.Schema.Types.ObjectId, ref: 'Class', required: true }, // <-- ADD THIS LINE
   answers: [{ type: Number }], // index of selected option for each question
   score: { type: Number },
   startedAt: { type: Date },
